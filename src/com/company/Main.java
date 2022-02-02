34dac6d2eadd1db49f7fa130e9e38c0ceb8de2ab
Nurzhyldyz <bekzanalievan@gmail.com>
2022-02-02 19:13:40 +0100

    Homework2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,39 +1,39 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Оставайтесь дома");
-        System.out.println("Оставайтесь дома");
-        System.out.println("Оставайтесь дома");
-        System.out.println("Можно идти гулять");
-        System.out.println("Можно идти гулять");
+        System.out.println(rec(30, 24));
+        System.out.println(rec(-45, 32));
+        System.out.println(rec(12, 89));
+        System.out.println(rec(13, 56));
+        System.out.println(rec(-12, 84));
+
+        System.out.println(generateRandomAge() + rec(generateRandomAge(),10));
     }
-    public static String multiplay () {
-        int ageOfMan = 35;
-        int temperature = 10;
-        if (ageOfMan >= 20 && ageOfMan <= 45 ||temperature >= -20 && temperature <= 30) {
-            System.out.println("Можно идти гулять");
 
-
-        }else if (ageOfMan <=20 || temperature >= 28 ) {
-            System.out.println("Можно идти гулять");
-
-        }else if (ageOfMan > 45 || temperature >= -10 && temperature <= 25) {
-            System.out.println("Можно идти гулять");
-        }else {
-            System.out.println("Оставайтесь дома");
-
-
+    public static String rec(int tempereture, int age) {
+        String toy = "Можно идти гулять";
+        String toy1 = "Нельзя идти гулять";
+        String toy2 = "Оставайтесь дома";
+        if (age >= 20 && age <= 45 && tempereture >= -20 && tempereture <= 30) {
+            return toy;
+        } else if (age <= 20 && tempereture > 0 && tempereture <= 28) {
+            return toy;
         }
-        return "Оставайтесь дома";
-    }
 
-    public static int generateRandomAge() {
-        int min = -40;
-        int max = 45;
-        int random_int = (int) Math.floor(Math.random() * (max - min + 1) + min);
-        return random_int;
+        else
+        {
+            return toy2;
+        }
+
+
+    }
+    public static int generateRandomAge(){
+        Random random = new Random();
+        int a = random.nextInt(10000);
+        return a;
     }
 }
-
